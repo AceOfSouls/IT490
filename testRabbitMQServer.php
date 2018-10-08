8d32c3d2cb3ae536bd8fb209bcc68a9a81a8594f
Sergio <linkmaster099@gmail.com>
2018-10-08 19:11:23 -0400

    get sign up to work with db and Dave

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -6,33 +6,52 @@ require_once('rabbitMQLib.inc');
 
 include ("account.php");
 
-$db = mysqli_connect($hostname, $username, $password, $project);
-global $db;
+$userdb = mysqli_connect($hostname, $username, $password, $project);
+global $userdb;
 
 if (mysqli_connect_errno())
   {
       echo "Failed to connect to MySQL: " . mysqli_connect_error();
       exit();
   }
-echo "Successfully connected to MySQL.  ";
+echo "Successfully connected to MySQL.".PHP_EOL;
 
 function auth ($user , $pass){
 
-    global $db;
+    global $userdb;
     $s = "SELECT * from UserLogin where username = \"$user\" && password = \"$pass\"";
-    $t = mysqli_query($db, $s);
+    $t = mysqli_query($userdb, $s);
 
     //echo "<br><br>SQL Statement: ".$s;
 
     if (mysqli_num_rows($t) == 0 )
         {
-            echo "User and Password combination not found.";
+            echo "User and Password combination not found.".PHP_EOL;
             return false;
         }
 
     else {
-        echo "Successfully Authenticated.";
+        echo "Successfully Authenticated.".PHP_EOL;
         return true;
+    }
+}
+
+function signup ($user, $pass, $email){
+    global $userdb;
+    $s = "SELECT * from UserLogin where username = \"$user\" || email = \"$email\"";
+    $t = mysqli_query($userdb, $s);
+
+    if (mysqli_num_rows($t) >= 1)
+    {
+	echo "User/email is already on database.".PHP_EOL;
+	return false;
+    }
+    else
+    {
+	$a = "INSERT INTO UserLogin(username,password,email) VALUES (\"$user\",\"$pass\",\"$email\")";
+	mysqli_query($userdb, $a);
+	echo "Successfully added User.".PHP_EOL;
+	return true;
     }
 }
 
@@ -48,6 +67,8 @@ function requestProcessor($request)
   {
     case "login":
       return auth($request['username'],$request['password']);
+    case "sign_up":
+      return signup($request['username'],$request['password'],$request['email']);
     case "validate_session":
       return doValidate($request['sessionId']);
   }
